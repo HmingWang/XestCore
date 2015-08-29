c6b5f311207a882303fe833c0457cd2cca81da05
王海明 <whaim1986@gmail.com>
2015-08-29 22:21:04 +0800

    123123

--- R.O.C.Kill/Main.cpp
+++ R.O.C.Kill/Main.cpp
@@ -2,17 +2,31 @@
 
 int main()
 {
-	Server s;
-	try
-	{
-		s.start();
-		s.run();
-	}
 
-	catch (std::string &e)
-	{
-		cout << e << endl;
-	}
+
+	cout << "系统环境：" << endl;
+#ifdef __GNUC__
+	cout << "__GNUC__" << endl;
+#endif
+#ifdef _MSC_VER
+	cout << "_MSC_VER" << endl;
+#endif
+#ifdef _WIN32
+	cout << "_WIN32" << endl;
+#endif
+#ifdef WIN32
+	cout << "WIN32" << endl;
+#endif
+#ifdef linux 
+	cout << "linux " << endl;
+#endif
+
+
+
+	Server s;
+	s.start();
+	s.run();
+
 
 
 	system("pause");
